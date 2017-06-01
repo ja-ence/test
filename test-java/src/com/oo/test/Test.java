9f8b39c839694060cf09b649e74327ddb7461c58
ja-ence <1518170287@qq.com>
2017-06-01 17:10:37 +0800

    new project for github test

---
+++ test-java/src/com/oo/test/Test.java
@@ -0,0 +1,14 @@
+package com.oo.test;
+
+public class Test {
+	private String test;
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getTest(){
+		return this.test;
+	}
+	public void setTest(String test){
+		this.test = test;
+	}
+}
