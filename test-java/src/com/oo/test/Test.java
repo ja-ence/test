84a789d005778e2bafd6d9eb1f9548ec78289920
ja-ence <1518170287@qq.com>
2017-06-02 17:33:24 +0800

    提价

--- test-java/src/com/oo/test/Test.java
+++ test-java/src/com/oo/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	private String test;
 	public Test() {
 		// TODO Auto-generated constructor stub
+		System.out.println("hello");
 	}
 	public String getTest(){
 		return this.test;
